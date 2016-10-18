9388f7ab4892eecfb112e35c3cc26a0c7864e8d3
xlcoder <xlcoder166@gmail.com>
2016-10-18 22:15:03 +0800

    18/19 Modify functions.php

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -3,6 +3,7 @@
  * @function 
  */
 
+//clean theme header load
 function hookScriptAndCss()
 {
   wp_enqueue_style( 'example-bootstrap', get_template_directory_uri() . '/inc/css/bootstrap.min.css' );
@@ -13,24 +14,30 @@ add_action("wp_enqueue_script", "hookScriptAndCss")
 
 function headerCleanup() 
 {
-    remove_action('wp_head', 'wlwmanifest_link'); // remove wlwmanifest.xml (needed to support windows live writer)
-    remove_action('wp_head', 'wp_generator'); // remove wordpress version
-    remove_action('wp_head', 'rsd_link'); // remove really simple discovery link
-    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0 ); // remove shortlink
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );  // remove emojis
     remove_action( 'wp_print_styles', 'print_emoji_styles' );   // remove emojis
-    remove_action('wp_head', 'adjacent_posts_rel_link_wp_head'); // remove the / and previous post links
-    remove_action('wp_head', 'feed_links', 2); // remove rss feed links
-    remove_action('wp_head', 'feed_links_extra', 3); // removes all extra rss feed links
-    remove_action( 'wp_head', 'rest_output_link_wp_head', 10 ); // remove the REST API link
-    remove_action( 'wp_head', 'wp_oembed_add_discovery_links' ); // remove oEmbed discovery links
-    remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 ); // remove the REST API link from HTTP Headers
-    remove_action( 'wp_head', 'wp_oembed_add_host_js' ); // remove oEmbed-specific javascript from front-end / back-end
-    remove_action('rest_api_init', 'wp_oembed_register_route'); // remove the oEmbed REST API route
-    remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10); // don't filter oEmbed results
+    remove_action('wp_head', 'feed_links_extra', 3); // Display the links to the extra feeds such as category feeds
+    remove_action('wp_head', 'feed_links', 2); // Display the links to the general feeds: Post and Comment Feed
+    remove_action('wp_head', 'rsd_link'); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+    remove_action('wp_head', 'wlwmanifest_link'); // Display the link to the Windows Live Writer manifest file.
+    remove_action('wp_head', 'wp_generator'); // Display the XHTML generator that is generated on the wp_head hook, WP version
+    remove_action('wp_head', 'rel_canonical');
+    remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
 }
 
 add_action('after_setup_theme', 'headerCleanup');
 
+//Modify excerpt length and more style
+function custom_excerpt_more( $more ) {
+    return "";
+}
 
+add_filter( 'excerpt_more', 'custom_excerpt_more' );
 
+function custom_excerpt_length( $length ) {
+	return 80;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+// Remove Admin bar
+add_filter('show_admin_bar', 'remove_admin_bar'); 
